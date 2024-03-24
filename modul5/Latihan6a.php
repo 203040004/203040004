583d489580bec3d883e5aa56159d79598c775dc2
203040004 <hasbulah.203040004@mail.unpas.ac.id>
2024-03-24 22:26:07 +0700

    mod pw 6

---
+++ modul5/Latihan6a.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grid of squares</title>
+    <style>
+        .container {
+            display: grid;
+        }
+        .square {
+            width: 50px;
+            height: 50px;
+            background-color: white;
+            border: 1px solid #000;
+            text-align: center;
+            line-height: 50px;
+        }
+        .blue {
+            width: 50px;
+            height: 50px;
+            background-color: #8B93FF;
+            border: 1px solid #000;
+            text-align: center;
+            line-height: 50px;
+        }
+        .line {
+            display: flex;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+    <?php
+    
+    if(isset($_GET['nama'])) {
+        $angka = $_GET['angka'];
+    } else {
+        $angka = 0;
+    }
+    
+for ($i = $angka; $i >= 1; $i--) {
+    echo '<div class="line">';
+    for ($j = 1; $j <= $i; $j++) {
+        if($i % 2 == 0){
+            echo '<div class="blue">' . $i . '</div>';
+        } else {
+            echo '<div class="square">' . $i . '</div>';
+        }
+        
+    }
+    echo '</div>';
+}
+?>
+       
+    </div>  
+
+</body>
+</html>
